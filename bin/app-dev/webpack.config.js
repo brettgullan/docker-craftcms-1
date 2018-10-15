9a231081112c5de15278e38f56d9fb2d8ec3e671
Brett Gullan <brettgullan@mac.com>
2018-10-15 15:28:15 +1100

    feature: Docker-based Webpack HMR
    Run webpack-dev-server from a Docker image.

---
+++ bin/app-dev/webpack.config.js
@@ -0,0 +1,34 @@
+
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: './dist',
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+}
